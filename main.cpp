4c08a99359852cf00f3e87fd3c1228f2329c0fb6
Benita Emudianughe <emusbeny@gmail.com>
2022-12-06 18:35:49 +0100

    C++ program to simply output random numbers
    Generating random numbers from initial user input, using the special keyword rand()

---
+++ main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+int main()
+{
+    // Outputs three random numbers
+    unsigned int seed;
+    int z1, z2, z3;
+    cout << "   --- Random Numbers  --- \n" << endl;
+    cout << "To initialize the random number generator, "
+         <<"\n please enter an integer value: ";
+    cin  >> seed;      // Input an integer
+    srand( seed);      // and use it as argument for a
+                       // new sequence of random numbers.
+
+    z1 = rand();       // Compute three random numbers.
+    z2 = rand();
+    z3 = rand();
+    cout << "\nThree random numbers: "
+         << z1 << "  " << z2 << "  " << z3 << "  " << endl;
+    return 0;
+}
